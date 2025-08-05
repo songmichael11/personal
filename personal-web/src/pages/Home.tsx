70dd73afbef17885f12da175bc3c6c985fe42d61
Michael Song <songmichael11@gmail.com>
2025-08-05 19:47:52 -0400

    fix:fixed TS errors

--- personal-web/src/pages/Home.tsx
+++ personal-web/src/pages/Home.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { useState } from "react";
-import { useEffect } from "react";
 import LandingScreen from "../components/LandingScreen";
 import NavBar from "../components/Navbar";
 import { About } from "../components/About";
