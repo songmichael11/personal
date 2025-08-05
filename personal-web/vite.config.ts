28e867eb2fdffd20efe76866623b978d3f0d0837
Michael Song <songmichael11@gmail.com>
2025-08-05 19:34:33 -0400

    feat:added vercel base path

--- personal-web/vite.config.ts
+++ personal-web/vite.config.ts
@@ -8,6 +8,7 @@ import flowbiteReact from "flowbite-react/plugin/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr(), flowbiteReact()],
+  base: process.env.VITE_BASE_PATH || "/personal",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
